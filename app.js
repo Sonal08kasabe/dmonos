98ed81a5fe5f66953f742497c2995306b5684d74
Sonal_kasabe <141398985+Sonal08kasabe@users.noreply.github.com>
2023-08-04 14:39:28 +0530

    Add files via upload
    domino's

---
+++ app.js
@@ -0,0 +1,35 @@
+/*Array Reversal: Implement a function to reverse the elements of an integer array in-place (without using additional arrays).
+Array Count: Write a function that counts and returns the number of elements in an integer array.
+Array Count: Write a function that counts and returns the number of occurrences of a specific element in an integer array.
+ar = [1,2,33,1,-1,15]*/ 
+
+/*let arr=[1,2,33,1,-1,15];
+const reverse=()=>{
+    arr.reverse();
+    console.log(arr);
+
+
+}
+console.log(reverse(arr));*/
+
+/*let arr=[1,2,33,1,-1,15];
+const count=()=>{
+  
+     return arr[0];
+    
+}
+console.log(count(arr));*/
+
+let arr=[1,2,33,1,-1,15]
+
+const Count=()=>{
+
+    for(let i=0;i<=arr.length;i++){
+     
+       return arr.length;
+    }
+
+
+}
+console.log((Count(arr)));
+
